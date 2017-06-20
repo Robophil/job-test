ca99073c68f542e1dfa776cb71e4601e27336da3
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-20 18:17:52 +0100

    flattened callback into promise chain

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -5,19 +5,22 @@ const print = result => console.log('Result:', result)
 const filterBooksWithTitleStartingWithA = (sortedBooks, callback) => {
   let filteredBooks
   /* filter logic here */
-  return callback(filteredBooks)
+  // return callback(filteredBooks)
+  return Promise.resolve(filteredBooks)
 }
 
 const sortBooksAlphabetically = (books, callback) => {
   let sortedBooks
   /* sorting logic books */
-  return callback(sortedBooks, print)
+  // return callback(sortedBooks, print)
+  return Promise.resolve(sortedBooks)
 }
 
 const getBooksAsync = (callback) => {
   setTimeout(() => {
-    callback(books, filterBooksWithTitleStartingWithA)
+    callback(books, null).then(sortedBooks => filterBooksWithTitleStartingWithA(sortedBooks, null).then(filteredBooks => print(filteredBooks)) )
   }, 2000)
+  
 }
 
 getBooksAsync(sortBooksAlphabetically)
