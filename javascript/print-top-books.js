6c5a7115e04975bd8bf0901e11bfab81d3124ce1
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-20 16:53:10 +0100

    import books

--- javascript/print-top-books.js
+++ javascript/print-top-books.js
@@ -2,9 +2,9 @@
  *  a module which exposes a function to find the top three books with the highest ratings, 
  *  and returns the contents of these books as an array of strings.
  */
-
+const books = require("./books")
 const orderedBooks =  require("./order-books-by-ratings")
 
 module.exports = () => {
-    return orderedBooks.slice(0, 3)
+    return orderedBooks(books).slice(0, 3)
 }
